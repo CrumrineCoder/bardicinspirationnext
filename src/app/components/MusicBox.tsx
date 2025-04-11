82edb153239735a6845cba2931ae64e6270c3eac
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-04-11 14:39:50 -0500

    react-youtube added

--- src/app/components/MusicBox.tsx
+++ src/app/components/MusicBox.tsx
@@ -1,37 +1,34 @@
+"use client";
 import React from "react";
+import YouTube from "react-youtube";
 
 interface MusicBoxProps {
-    songName: string;
-    tags: string[];
-    link: string;
+  songName: string;
+  tags: string[];
+  link: string;
 }
 
+const options = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      autoplay: 1,
+      controls: 1,
+    },
+  };
+
 const MusicBox: React.FC<MusicBoxProps> = ({ songName, tags, link }) => {
-    return (
-        <div className="MusicBox">
-            <h2 className="MusicBoxHeader">{songName}</h2>
-            
-            <div className="MusicBoxVideo">
-                <iframe
-                    width="560"
-                    height="315"
-                    src={link}
-                    title={songName}
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                ></iframe>
-            </div>
-            
-            <div className="MusicBoxTags">
-                {tags.map((tag, index) => (
-                    <span key={index} className="MusicBoxTag">
-                        {tag}
-                    </span>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="MusicBox">
+      <h2 className="MusicBoxHeader">{songName}</h2>
+      <YouTube videoId={link} opts={options} />
+      {tags.map((tag, index) => (
+        <span key={index}>
+          {tag}
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default MusicBox;
