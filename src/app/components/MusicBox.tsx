e2d39ca720ea8ff7fadc8b1e8755053b124915ab
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-04-11 16:43:07 -0500

    33

--- src/app/components/MusicBox.tsx
+++ src/app/components/MusicBox.tsx
@@ -3,33 +3,42 @@ import React from "react";
 import YouTube from "react-youtube";
 
 interface MusicBoxProps {
-    songName: string;
-    tags: string[];
-    link: string;
-    artist: string;
+  songName: string;
+  tags: string[];
+  link: string;
+  artist: string;
 }
 
 const options = {
-    playerVars: {
-        autoplay: 1,
-        controls: 1,
-    },
+  playerVars: {
+    autoplay: 1,
+    controls: 1,
+  },
 };
 
-const MusicBox: React.FC<MusicBoxProps> = ({ songName, tags, link, artist }) => {
-    return (
-        <div className="MusicBox">
-            <h2 className="MusicBoxHeader">{artist} - {songName}</h2>
-            <div className="youtubeWrapper">
-                <YouTube videoId={link} opts={options} />
-            </div>
-            {tags.map((tag, index) => (
-                <span key={index}>
-                    {tag}
-                </span>
-            ))}
-        </div>
-    );
+const MusicBox: React.FC<MusicBoxProps> = ({
+  songName,
+  tags,
+  link,
+  artist,
+}) => {
+  return (
+    <div className="MusicBox">
+      <h2 className="MusicBoxHeader">
+        {artist} - {songName}
+      </h2>
+      <div className="youtubeWrapper">
+        <YouTube videoId={link} opts={options} />
+      </div>
+      <div className="tagWrapper">
+        {tags.map((tag, index) => (
+          <span className="tag" key={index}>
+            {tag}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MusicBox;
