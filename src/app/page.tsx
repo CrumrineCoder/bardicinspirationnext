8fe366862b9fd6d9e0776f9faaa80a1503d089de
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-04-13 03:10:36 -0500

    Reworking from base principles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,37 @@
 'use client';
-import MusicBox from "./components/MusicBox";
-import songs from "./fakeDB/songs";
-import users from "./fakeDB/user";
-import { useState, useEffect } from "react";
+//import MusicBox from "./components/MusicBox";
+
+//import songs from "./fakeDB/songs";
+
+//import users from "./fakeDB/user";
+import songData from "./fakeDB/songData";
+//import tags from "./fakeDB/tags";
+
+//import { useState, useEffect } from "react";
 
 import "./app.scss";
 
 
 export default function Home() {
   // To be replaced with JWT? logic for getting the actual user 
-  const [currentUser, setCurrentUser] = useState<{ id: number; email: string; userName: string } | null>(null);
+/*  const [currentUser, setCurrentUser] = useState<{ id: number; email: string; userName: string } | null>(null);
   useEffect(() => {
     setCurrentUser(users[0]);
   }, []);
+  */
   //
-  
+
   return (
     <div className="MusicBoxContainer">
-      {songs.map((song, index) => (
+      {songData.map((song, index) => (
+        <div className="song" key={index}>{song.songName}</div>
+      ))}
+ 
+    </div>
+  );
+}
+/*
+{songs.map((song, index) => (
         <MusicBox
           key={index}
           songName={song.songName}
@@ -27,6 +41,4 @@ export default function Home() {
           user={currentUser}
         />
       ))}
-    </div>
-  );
-}
+      */
