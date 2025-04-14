a46fa7e353b331e4a2ae515e339f80dbe72d8124
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-04-14 05:37:18 -0500

    Mock login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import MusicBox from "./components/MusicBox";
 
 //import songs from "./fakeDB/songs";
 
-
 import songData from "./fakeDB/songData";
 import tags from "./fakeDB/tags";
 import songTags from "./fakeDB/songTags";
@@ -17,7 +16,7 @@ import "./app.scss";
 
 export default function Home() {
   //const [currentTag] = useState();
-  const [currentUserID] = useState(null);
+  const [currentUserID, setCurrentUserID] = useState<number | null>(null);
   // To be replaced with JWT? logic for getting the actual user
   /*  const [currentUser, setCurrentUser] = useState<{ id: number; email: string; userName: string } | null>(null);
   useEffect(() => {
@@ -27,36 +26,43 @@ export default function Home() {
   //
 
   return (
-    <div className="MusicBoxContainer">
-      {songData.map((song, index) => {
-        // This is logic for getting the current tags for the song. 
+    <>
 
-        // to do: can I speed this up by not having to search all of songTags for each song? 
-        // Get all SongTags (relational map) for currently visible Songs. Will not include songs that are not visible (Deleted songs on initial load)
-        const SongTagsIDs = songTags.filter(
-          (songTag) => songTag.songId === song.id
-        );
-        /*
+    <button onClick={() => setCurrentUserID(1)}>Login</button>
+    <br />
+    <button onClick={() => setCurrentUserID(null)}>Logout</button>
+    <br />
+    Current User:{""}{currentUserID}
+      <div className="MusicBoxContainer">
+        {songData.map((song, index) => {
+          // This is logic for getting the current tags for the song.
+
+          // to do: can I speed this up by not having to search all of songTags for each song?
+          // Get all SongTags (relational map) for currently visible Songs. Will not include songs that are not visible (Deleted songs on initial load)
+          const SongTagsIDs = songTags.filter(
+            (songTag) => songTag.songId === song.id
+          );
+          /*
          { id: 1, songId: 1, tagId: 1 },
          { id: 2, songId: 1, tagId: 2 },
          { id: 3, songId: 1, tagId: 3 }
         */
-        // Using the SongTags relational map, get all Tag IDs
-        const currentSongsTagIDs = SongTagsIDs.map((SongTags) => ({
-          tagID: SongTags.tagId,
-        }));
-        // [ { tagID: 1 }, { tagID: 2 }, { tagID: 3 } ]
+          // Using the SongTags relational map, get all Tag IDs
+          const currentSongsTagIDs = SongTagsIDs.map((SongTags) => ({
+            tagID: SongTags.tagId,
+          }));
+          // [ { tagID: 1 }, { tagID: 2 }, { tagID: 3 } ]
 
-        const reducedCurrentTagsIDs = currentSongsTagIDs.map(
-          ({ tagID }) => tagID
-        );
-        // [ 1, 2, 3]
-        // [ 1 ]
+          const reducedCurrentTagsIDs = currentSongsTagIDs.map(
+            ({ tagID }) => tagID
+          );
+          // [ 1, 2, 3]
+          // [ 1 ]
 
-        const currentTags = tags.filter((taglisting) =>
-          reducedCurrentTagsIDs.includes(taglisting.id)
-        );
-        /*
+          const currentTags = tags.filter((taglisting) =>
+            reducedCurrentTagsIDs.includes(taglisting.id)
+          );
+          /*
         [
           { id: 1, name: 'calm' },
           { id: 2, name: 'melancholic' },
@@ -64,22 +70,20 @@ export default function Home() {
         ]
         */
 
-        // From each TagID, get the name of the Tag.
-        // Deprecated: we get the currentTagNames when looping through SongTags
-        // const currentTagNames = currentTags.map(({ name }) => name);
-        // [ 'calm', 'melancholic', 'piano' ]
+          // From each TagID, get the name of the Tag.
+          // Deprecated: we get the currentTagNames when looping through SongTags
+          // const currentTagNames = currentTags.map(({ name }) => name);
+          // [ 'calm', 'melancholic', 'piano' ]
 
-        const reducedCurrentSongsTagsIDs = SongTagsIDs.map(
-          ({ id }) => id
-        );
-        // [ 1, 2, 3 ]
-        // [ 4 ] (It always scales up, unlike the tags, because they're all unique IDs. not to say that it will be 1 2 3 4, but to show different answers as compared to tags).
+          const reducedCurrentSongsTagsIDs = SongTagsIDs.map(({ id }) => id);
+          // [ 1, 2, 3 ]
+          // [ 4 ] (It always scales up, unlike the tags, because they're all unique IDs. not to say that it will be 1 2 3 4, but to show different answers as compared to tags).
 
-        // Search for tagVotes by the reduced CurrentSongTagsIDs (songTagID). This has the userID. 
-        const currentTagVotes = tagVotes.filter((tagVote) =>
-          reducedCurrentSongsTagsIDs.includes((tagVote.songTag))
-        );
-        /*
+          // Search for tagVotes by the reduced CurrentSongTagsIDs (songTagID). This has the userID.
+          const currentTagVotes = tagVotes.filter((tagVote) =>
+            reducedCurrentSongsTagsIDs.includes(tagVote.songTag)
+          );
+          /*
         [
           { id: 1, userID: 1, songTag: 1 },
           { id: 2, userID: 2, songTag: 2 },
@@ -90,9 +94,9 @@ export default function Home() {
           { id: 7, userID: 3, songTag: 3 },
           { id: 8, userID: 4, songTag: 1 }
         ]
-        */ 
-        // Create a mapping of tags to the users who voted for them
-        /* 
+        */
+          // Create a mapping of tags to the users who voted for them
+          /* 
          [
           { id: 1, name: 'calm' },
           { id: 2, name: 'melancholic' },
@@ -100,45 +104,44 @@ export default function Home() {
         ]
           Looping over each object in this array
         */
-        const tagsWithVoters = currentTags.map((tag) => {
-          // { id: 1, name: 'calm' }
-          // Find the SongTags entry for this tag
-          const songTag = SongTagsIDs.find((songTag) => songTag.tagId === tag.id);
-          // { id: 1, songId: 1, tagId: 1 }
+          const tagsWithVoters = currentTags.map((tag) => {
+            // { id: 1, name: 'calm' }
+            // Find the SongTags entry for this tag
+            const songTag = SongTagsIDs.find(
+              (songTag) => songTag.tagId === tag.id
+            );
+            // { id: 1, songId: 1, tagId: 1 }
 
-          // Find all votes for this SongTag
-          const voterIDs = currentTagVotes
-            .filter((tagVote) => tagVote.songTag === songTag?.id)
-            .map((tagVote) => tagVote.userID);
+            // Find all votes for this SongTag
+            const voterIDs = currentTagVotes
+              .filter((tagVote) => tagVote.songTag === songTag?.id)
+              .map((tagVote) => tagVote.userID);
 
-          // Loop over each VoterID, and for each one search for the first user which has the same ID (should only be 1 per the DB, and then return its email)
-          const voterNames = voterIDs
-            .map((voter) => users.find(user => user.id === voter)?.email)
-            .filter((email): email is string => email !== undefined);
-   
-          return {
-            tagName: tag.name,
-            voters: voterNames,
-          };
-        });
- 
+            // Loop over each VoterID, and for each one search for the first user which has the same ID (should only be 1 per the DB, and then return its email)
+            const voterNames = voterIDs
+              .map((voter) => users.find((user) => user.id === voter)?.email)
+              .filter((email): email is string => email !== undefined);
 
-        // const currentTagVotes = tagVotes.filter((tagVote) => tagVote.includes(reducedCurrentSongsTagsIDs)).
+            // When validating users, I might want to send the whole user instead of just the names unless names are unique (But IDs might be safer? In case a user deletes their account?)
+            return {
+              tagName: tag.name,
+              voters: voterNames,
+            };
+          });
 
-        // For returned tagVotes, return the userIDs so that we have the people who voted on the tags below. 
-
-        return (
-          <div className="song" key={index}>
-            <MusicBox
-              key={index}
-              songName={song.songName}
-              link={song.link}
-              tags={tagsWithVoters}
-              artist={song.artist}
-            />
-          </div>
-        );
-      })}
-    </div>
+          return (
+            <div className="song" key={index}>
+              <MusicBox
+                key={index}
+                songName={song.songName}
+                link={song.link}
+                tags={tagsWithVoters}
+                artist={song.artist}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
