2b42dfd44800e9e32950021d5c2002d97483d37e
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-04-15 18:27:11 -0500

    Update the SQL queries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,13 @@
 'use client'
-import HomePage from "./clientContainers/HomePage";
-
-import { songsTable } from "./db/schema";
 import SongList from "./serverComponents/SongList";
 import SendDataButton from "./clientContainers/sendDataButton";
-
-import { db } from "./db/uplink";
 
 import {useState} from "react";
 
 export default function Home() {
   const [refresh, setRefresh] = useState(false);
 
-  // Function to toggle the refresh state
+  // Refreshes songlist by changing state, triggering a refresh. 
   const triggerRefresh = () => setRefresh((prev) => !prev);
 
   return (
--- src/app/queries/createTable.sql
+++ src/app/queries/createTable.sql
@@ -3,3 +3,25 @@ CREATE TABLE songs (
         songName TEXT NOT NULL,
             artist TEXT NOT NULL,
                 link TEXT NOT NULL)
+
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY,
+        tagName TEXT NOT NULL)
+
+CREATE TABLE songTags (
+    id SERIAL PRIMARY KEY,
+        songID INTEGER NOT NULL,
+            tagID INTEGER NOT NULL
+            )
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+        email TEXT NOT NULL,
+            userName TEXT NOT NULL
+            )
+
+CREATE TABLE tagVotes (
+    id SERIAL PRIMARY KEY,
+        userID INTEGER NOT NULL,
+            songTagID INTEGER NOT NULL
+            )
