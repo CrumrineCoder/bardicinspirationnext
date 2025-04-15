c755263c6236b5f803849828e10c45e76f52e0ad
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-04-15 02:03:24 -0500

    oops typo

--- src/app/db/schema.ts
+++ src/app/db/schema.ts
@@ -2,7 +2,7 @@ import {  text, serial, pgTable } from "drizzle-orm/pg-core";
 
 export const songsTable = pgTable("songs", {
     id: serial("id").primaryKey().notNull(),
-    songName: text("songName").notNull(),
+    songName: text("songname").notNull(),
     artist: text("artist").notNull(),
     link: text("link").notNull(),
 });
