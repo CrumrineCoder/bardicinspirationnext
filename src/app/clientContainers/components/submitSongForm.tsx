b9bb6d663da3644e74e4e7c489d00f11a3753a73
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-04-18 04:33:52 -0500

    horizontally center form

--- src/app/clientContainers/components/submitSongForm.tsx
+++ src/app/clientContainers/components/submitSongForm.tsx
@@ -8,49 +8,55 @@ export default function SubmitSongForm({ onUpdate }: { onUpdate: () => void }) {
   const [link, setLink] = useState<string | null>(null);
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        if (artist && songName && link) {
-          addSongToDB(songName, artist, link);
-          onUpdate();
-          setSongName(null);
-          setArtist(null);
-          setLink(null);
-        }
-      }}
-    >
-      <div className="formInputContainer">
-        <label htmlFor="songName">Enter Song Name: </label>
-        <input
-          type="text"
-          name="songName"
-          required
-          value={songName || ""}
-          onChange={(e) => setSongName(e.target.value)}
-        />
-      </div>
-      <div className="formInputContainer">
-        <label htmlFor="artist">Enter Artist Name: </label>
-        <input
-          type="text"
-          name="artist"
-          required
-          value={artist || ""}
-          onChange={(e) => setArtist(e.target.value)}
-        />
-      </div>
-      <div className="formInputContainer">
-        <label htmlFor="link">Enter Song Link: </label>
-        <input
-          type="text"
-          name="link"
-          required
-          value={link || ""}
-          onChange={(e) => setLink(e.target.value)}
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+    <div className="SubmitFormJumbo">
+      <form
+        className="SubmitFormContainer"
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (artist && songName && link) {
+            addSongToDB(songName, artist, link);
+            onUpdate();
+            setSongName(null);
+            setArtist(null);
+            setLink(null);
+          }
+        }}
+      >
+        <div className="formInputContainer">
+          <label htmlFor="songName"></label>
+          <input
+            type="text"
+            name="songName"
+            required
+            placeholder="Enter Song Name"
+            value={songName || ""}
+            onChange={(e) => setSongName(e.target.value)}
+          />
+        </div>
+        <div className="formInputContainer">
+          <label htmlFor="artist"></label>
+          <input
+            type="text"
+            name="artist"
+            required
+            placeholder="Enter Artist Name"
+            value={artist || ""}
+            onChange={(e) => setArtist(e.target.value)}
+          />
+        </div>
+        <div className="formInputContainer">
+          <label htmlFor="link"></label>
+          <input
+            type="text"
+            name="link"
+            required
+            placeholder="Enter Song Link"
+            value={link || ""}
+            onChange={(e) => setLink(e.target.value)}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
