73069c3cc7067acf38271647f320a41e2e4c5722
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-04-26 18:24:28 -0500

    integrating cleark

--- src/app/authenticationComponents/CurrentUserContext.tsx
+++ src/app/authenticationComponents/CurrentUserContext.tsx
@@ -1,9 +1,38 @@
-
 import { User } from "../interfaces";
-import {useState, createContext} from "react"
+import { useState, createContext } from "react";
+import { getCurrentCookie, logout } from "../session";
 
-export const CurrentUserContext = createContext(null);
-
-export const CurrentUserProvider = ({ children }: { children: React.ReactNode }) => {
-    const [currentUser, setCurrentUser] = 
+interface AuthContextType {
+  userID: number | null;
+  checkUser: () => Promise<void>;
+  logoutUser: () => Promise<void>;
 }
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const CurrentUserProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [userID, setUserID] = useState<number | null>(null);
+
+  async function checkUser() {
+    const user = await getCurrentCookie();
+    if (
+      user &&
+      typeof user === "object" &&
+      "userId" in user &&
+      typeof user.userId === "number"
+    ) {
+      setUserID(user.userId);
+    } else {
+      setUserID(null);
+    }
+  }
+
+  async function logoutUser() {
+    await logout();
+    setUserID(null);
+  }
+};
