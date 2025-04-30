e8e1fc9bc19806c9abe1acfce1779237964dd2ac
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-04-29 20:32:26 -0500

    search by tag does technically work again

--- src/app/SongDiscovery/page.tsx
+++ src/app/SongDiscovery/page.tsx
@@ -2,12 +2,13 @@
 import { useState, useEffect } from "react";
 import { Song, Tag } from "../interfaces";
 import CurrentSong from "./CurrentSong";
-import { fetchAllSongs, fetchAllTags } from "../queries/fetchData";
+import { fetchAllSongs, fetchAllTags, fetchSongsByTagName } from "../queries/fetchData";
 
 export default function SongDiscovery() {
   const [selectedSong, setSelectedSong] = useState<Song | null>(null);
   const [selectedTag, setSelectedTag] = useState<Tag | null>(null);
   const [allSongs, setAllSongs] = useState<Song[] | null>(null);
+  const [tagName, setTagName] = useState<string | null>(null);
 
   useEffect(() => {
     fetchAllSongs().then(async (response) => {
@@ -22,22 +23,64 @@ export default function SongDiscovery() {
   useEffect(() => {}, [selectedSong]);
 
   return (
-    <div className="relative flex gap-5">
-      <div>
-        {allSongs &&
-          allSongs.map((song, index) => (
-            <div
-              className={`SongListing py-1 ${selectedSong?.id === song.id ? "pl-5 ActiveSongListing" : ""}`}
-              onClick={() => {
-                setSelectedSong(song);
-              }}
-              key={index}
-            >
-              {song.songName}
-            </div>
-          ))}
+    <div>
+      <form
+        className="SearchTagForm"
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (tagName) {
+            fetchSongsByTagName(tagName.toLowerCase())
+              .then((data) => {
+                setAllSongs(data);
+              })
+              .catch((error) => {
+                console.error("Error fetching songs by tag name:", error);
+                // Need to add a disclaimer to show that the tag isn't in the DB
+                // Also suggested tags, autocomplete, etc.
+              });
+          }
+        }}
+      >
+        <input
+          type="text"
+          name="tagName"
+          placeholder="Enter Tag"
+          required
+          value={tagName || ""}
+          onChange={(e) => setTagName(e.target.value)}
+          className="SearchTagBar"
+        />
+        <button className="SearchTagSubmitButton" type="submit">
+          Search Tag
+        </button>
+      </form>
+      <button
+        className="SearchTagResetButton"
+        onClick={() => {
+          setTagName(null);
+        }}
+      >
+        Reset Filter
+      </button>
+      <div className="relative flex gap-5">
+        <div>
+          {allSongs &&
+            allSongs.map((song, index) => (
+              <div
+                className={`SongListing py-1 ${
+                  selectedSong?.id === song.id ? "pl-5 ActiveSongListing" : ""
+                }`}
+                onClick={() => {
+                  setSelectedSong(song);
+                }}
+                key={index}
+              >
+                {song.songName}
+              </div>
+            ))}
+        </div>
+        {selectedSong && <CurrentSong song={selectedSong}></CurrentSong>}
       </div>
-      {selectedSong && <CurrentSong song={selectedSong}></CurrentSong>}
     </div>
   );
 }
