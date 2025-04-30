b6c4d4859b776a76e6c74e1344b36bbe0c4845e5
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-04-30 08:23:38 -0500

    fixing structure

--- src/app/SongDiscovery/page.tsx
+++ src/app/SongDiscovery/page.tsx
@@ -13,7 +13,6 @@ export default function SongDiscovery() {
   const [selectedTag, setSelectedTag] = useState<string | null>(null);
   const [allSongs, setAllSongs] = useState<Song[] | null>(null);
   const [allTags, setAllTags] = useState<string[] | null>(null);
-  const [tagName, setTagName] = useState<string | null>(null);
 
   function getAllSongs() {
     fetchAllSongs().then(async (response) => {
@@ -43,13 +42,13 @@ export default function SongDiscovery() {
     getAllTags();
   }, []);
 
-  useEffect(() => {
+  function searchTag() {
     if (selectedTag) {
       fetchSongsByTagName(selectedTag).then(async (response) => {
         setAllSongs(response);
       });
     }
-  }, [selectedTag]);
+  }
 
   return (
     <div className="relative flex text-white gap-10">
@@ -71,56 +70,64 @@ export default function SongDiscovery() {
       </div>
       {selectedSong && <CurrentSong song={selectedSong}></CurrentSong>}
       <div>
-        <form
-          className=""
-          onSubmit={(e) => {
-            e.preventDefault();
-            if (tagName) {
-              fetchSongsByTagName(tagName.toLowerCase())
-                .then((data) => {
-                  setAllSongs(data);
-                })
-                .catch((error) => {
-                  console.error("Error fetching songs by tag name:", error);
-                  // Need to add a disclaimer to show that the tag isn't in the DB
-                  // Also suggested tags, autocomplete, etc.
-                });
-            }
-          }}
-        >
-          <input
-            type="text"
-            name="tagName"
-            placeholder="Enter Tag"
-            required
-            value={tagName || ""}
-            onChange={(e) => setTagName(e.target.value)}
-            className="mr-2 p-1"
-          />
-          <button className="" type="submit">
-            Search Tag
-          </button>
-          <div>
-            <button
-              className=""
-              onClick={() => {
-                setTagName(null);
-                getAllSongs();
+        <div>
+          <form
+            className=""
+            onSubmit={(e) => {
+              e.preventDefault();
+              if (selectedTag) {
+                fetchSongsByTagName(selectedTag.toLowerCase())
+                  .then((data) => {
+                    setAllSongs(data);
+                  })
+                  .catch((error) => {
+                    console.error("Error fetching songs by tag name:", error);
+                    // Need to add a disclaimer to show that the tag isn't in the DB
+                    // Also suggested tags, autocomplete, etc.
+                  });
+              }
+            }}
+          >
+            <input
+              type="text"
+              name="tagName"
+              placeholder="Enter Tag"
+              required
+              value={selectedTag || ""}
+              onChange={(e) => {
+                e.preventDefault();
+                setSelectedTag(e.target.value);
               }}
+              className="mr-2 p-1"
+            />
+            <button
+              onClick={() => searchTag()}
+              className="SmallButton"
+              type="submit"
             >
-              Reset Filter
+              Search Tag
             </button>
-          </div>
-        </form>
+          </form>
+          <button
+            className="SmallButton"
+            onClick={() => {
+              setSelectedTag(null);
+              getAllSongs();
+            }}
+          >
+            Reset Filter
+          </button>
+        </div>
         {allTags && (
-          <div>
+          <div className="text-center">
             <h4>Recent Tags</h4>
             {allTags.map((tag, index) => (
               <span
-                onClick={() => {
+                key={index}
+                onClick={async () => {
                   setSelectedTag(tag);
                 }}
-                className="pr-2 ClickableInlineEntry"
+                className="pr-2 ClickableInlineEntry SidebarTagListing"
               >
                 {tag}
               </span>
