7712f244a820b7b4fe76cc96e072a36c023da021
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-05-08 13:43:56 -0500

    Min width

--- src/app/SongDiscovery/SongListing.tsx
+++ src/app/SongDiscovery/SongListing.tsx
@@ -15,39 +15,39 @@ export default function SongListing({
   disabled,
 }: SongListingProps) {
   return (
-    <div className={`${disabled ? "disabled" : ""} w-48`}>
+    <div className={`${disabled ? "disabled" : ""} w-48 min-w-32`}>
       <div className="flex justify-center mb-3">
-        <button
-          className="SmallButton"
-          onClick={() => {
-            if (allSongs && allSongs.length > 0) {
-              const randomIndex = Math.floor(Math.random() * allSongs.length);
-              setSelectedSong(allSongs[randomIndex]);
-            }
-          }}
-        >
-          Random Song
-        </button>
+      <button
+        className="SmallButton"
+        onClick={() => {
+        if (allSongs && allSongs.length > 0) {
+          const randomIndex = Math.floor(Math.random() * allSongs.length);
+          setSelectedSong(allSongs[randomIndex]);
+        }
+        }}
+      >
+        Random Song
+      </button>
       </div>
       {allSongs &&
-        allSongs.map((song, index) => (
-          <div
-            className={`ClickableInlineEntry ${
-              selectedSong?.id === song.id ? "pl-2 ActiveSongListing" : ""
-            } leading-tight pb-2`}
-            onClick={() => {
-              setSelectedSong(song);
-            }}
-            key={index}
-          >
-            {song.songName}{" "}
-            {song.version && (
-              <span className="opacity-50 block">
-                [{song.version} version]
-              </span>
-            )}
-          </div>
-        ))}
+      allSongs.map((song, index) => (
+        <div
+        className={`ClickableInlineEntry ${
+          selectedSong?.id === song.id ? "pl-2 ActiveSongListing" : ""
+        } leading-tight pb-2`}
+        onClick={() => {
+          setSelectedSong(song);
+        }}
+        key={index}
+        >
+        {song.songName}{" "}
+        {song.version && (
+          <span className="opacity-50 block">
+          [{song.version} version]
+          </span>
+        )}
+        </div>
+      ))}
     </div>
   );
 }
