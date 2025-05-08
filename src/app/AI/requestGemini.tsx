e64fbc012ffdb5d45be9ea7dd99161d797965a6e
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-05-08 11:54:44 -0500

    Comments

--- src/app/AI/requestGemini.tsx
+++ src/app/AI/requestGemini.tsx
@@ -3,6 +3,7 @@ import { addTagToDB } from "../queries/addToDB";
 import { Button } from "@/components/ui/button";
 import { useEffect } from "react";
 
+// Used in the Song Discovery tab for retrieving and displaying tag results parsed by AI
 export default function requestGemini({
   onUpdate,
   songID,
@@ -18,11 +19,15 @@ export default function requestGemini({
   tags: string[];
   allTags?: string[] | null;
 }) {
+  // The AI will be returning a list of strings, without identifiers because the AI might suggest new tags. We could be storing them by Tags, but the AI is also prompted to add a few new tags so it'd be more work to sort them by existing tags and new tags which is unreliable. 
   const [AITags, setAITags] = useState<string[] | null>(null);
 
+  // Everytime the song (songid) changes, we need to wipe this component
   useEffect(() => {
     setAITags(null);
   }, [songID]);
+
+  // Get AI-suggested tags via API
   async function requestGeminiAPI() {
     const response = await fetch(
       `/api/requestGemini?songName=${encodeURIComponent(
@@ -45,13 +50,16 @@ export default function requestGemini({
     }
   }
 
+  // Functionality for when the user clicks on an AI-suggested tag to add to the DB
   async function submitAITag(tagName: string, index: number) {
     if (tagName && AITags) {
       try {
         await addTagToDB(tagName.toLowerCase(), songID);
+        // Remove AI tag from list of AI suggestions
         let newArray = AITags;
         delete newArray[index];
         setAITags(newArray);
+        // Refreshes the parent component
         onUpdate();
       } catch (error: unknown) {
         console.error("Error adding tag to database:", error);
@@ -59,6 +67,7 @@ export default function requestGemini({
     }
   }
 
+  // For each AI tag, we'll create a small component that will allow the user to add that tag, or if there's no AI tags the user will be prompted to start the API call
   return (
     <div className="mt-2">
       {AITags ? (
