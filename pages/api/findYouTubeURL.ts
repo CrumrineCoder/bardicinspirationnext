be1383308c76fe43f81b17442a356c7de13de870
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-05-08 10:23:10 -0500

    Update findYouTubeURL.ts

--- pages/api/findYouTubeURL.ts
+++ pages/api/findYouTubeURL.ts
@@ -1,5 +1,7 @@
 import type {NextApiRequest, NextApiResponse} from "next";
 
+// Using the YouTube Data API, we'll search by the user's query to return the top 5 YouTube videos
+// Input: songName, artist
 export default async function findYouTubeURL(
     req: NextApiRequest, 
     res: NextApiResponse
@@ -8,12 +10,15 @@ export default async function findYouTubeURL(
         const searchQuery  = req.query;
 
         //  query: { songName: 'City of Tears', artist: 'Hollow Knight' },
+        // Validate the query
         if (!searchQuery || (!searchQuery.songName || !searchQuery.artist)) {
             res.status(400).json({ error: "Missing search query" });
             return;
         }
         const apiKey = process.env.YOUTUBE_API_KEY;
 
+        // Construct the YouTube Data API search URL
+        // The query combines the song name and artist, appending "song" for better results
         const youtubeApiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(
              (typeof searchQuery.songName === "string" ? searchQuery.songName : "") + 
              (typeof searchQuery.artist === "string" ? searchQuery.artist : "") + " song"
@@ -24,6 +29,7 @@ export default async function findYouTubeURL(
             throw new Error("Failed to fetch data from YouTube API");
         }
 
+        // Extract the id of each video. We're returning just the URL id because these IDs are used (atm) just for YouTube iframes. If we needed more complex data from YouTube, we might return a more curated object
         const result = await response.json();
         const videoIDs = result.items
             .filter((item: any) => item.id && item.id.videoId)
