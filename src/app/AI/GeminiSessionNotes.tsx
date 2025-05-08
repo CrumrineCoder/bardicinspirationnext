19beef50dd92a154348f63b834843c6ffddccefe
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-05-08 11:40:51 -0500

    comments

--- src/app/AI/GeminiSessionNotes.tsx
+++ src/app/AI/GeminiSessionNotes.tsx
@@ -1,29 +1,42 @@
 import { useState } from "react";
 import { fetchAllTags } from "../queries/fetchData";
 import { Textarea } from "@/components/ui/textarea";
-import GeminiTagresults from "./GeminiTagResults";
-import { GeminiTagResultsProps } from "../interfaces";
 
+// List of Songs based on a tag to show to the user in a Gallery
+import GeminiTagresults from "./GeminiTagResults";
+
+import { GeminiTagResultsProps } from "../interfaces";
+//  tag: string;
+//  reason: string;
+
+// Component takes DM's session notes and makes an API call to generate Tag Results (which will have the corresponding songs within)
 export default function GeminiSessionNotes() {
+  // String we'll be sending to the AI, edited by the user. 
   const [userInput, setUserInput] = useState<string>(
     "Add your session notes here!"
   );
+  // Array of Objects sent by the AI api
   const [AIResponse, setAIResponse] = useState<GeminiTagResultsProps[] | null>(
     null
   );
 
+  // Handle user changes to the textarea element
   const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setUserInput(e.target.value);
   };
 
   async function queryAI() {
+    // Clear current response
     setAIResponse(null);
+    // Get every tag in the Database
     fetchAllTags().then(async (response) => {
       const data = await response;
       if (data) {
+        // Get the TagName from each tag
         const mappedData = data.map((tag: { tagName: string }) => {
           return tag.tagName;
         });
+        // Then make an array to send to the API
         const joinedTags = mappedData.join(",");
         const response = await fetch(
           `/api/geminiSuggestTags?sessionNotes=${encodeURIComponent(
@@ -38,6 +51,7 @@ export default function GeminiSessionNotes() {
         );
         if (await response.ok) {
           const data = await response.json();
+          // We'll be getting a JSON from the API here, which can be iterated upon to make the GeminiTagResults
           setAIResponse(data.tags);
         }
       }
