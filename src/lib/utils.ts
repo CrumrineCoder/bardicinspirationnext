d4860d031a7f8d182171bb89f79ced349e46395e
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-05-08 10:17:08 -0500

    Starting to comment/readme

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// twMerge will handle any conflicts with classNames from Tailwind in-line classes in JSX
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
