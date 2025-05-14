269057fe8fafdab0d2c2a9ca0f8bc1fd6b777ce2
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-05-14 11:13:32 -0500

    Update page.test.tsx

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -2,7 +2,7 @@ import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Page from "../src/app/page";
 
-test("Page", () => {
+test("Landing Page Loads", () => {
   render(<Page />);
   expect(screen.getByRole("link", { name: "Find More Songs" })).toBeDefined();
   expect(screen.getByTestId("landing-container")).toBeDefined();
